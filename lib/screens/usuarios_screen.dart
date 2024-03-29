a0ede559f5be62c64982ea55599649c749c62f49
Alison Puerta <131219053+YisethVelez000@users.noreply.github.com>
2024-03-29 16:20:56 -0500

    Modificamos el listar para que edite

--- lib/screens/usuarios_screen.dart
+++ lib/screens/usuarios_screen.dart
@@ -9,14 +9,12 @@ class UsuariosScreen extends StatefulWidget {
   State<UsuariosScreen> createState() => _UsuariosScreenState();
 }
 
-class EditarUsuarios{
-
+class EditarUsuarios {
   final String name;
   final String email;
   final String rol;
 
   EditarUsuarios({
-
     required this.name,
     required this.email,
     required this.rol,
@@ -32,7 +30,8 @@ class EditarUsuarios{
 
   //Realizamos el fetch a la API
   Future<EditarUsuarios> fetchEditarUsuarios() async {
-    final response = await http.get(Uri.parse('https://apiusuarios-copia.onrender.com/api/user/listarUsuarios'));
+    final response = await http.get(Uri.parse(
+        'https://apiusuarios-copia.onrender.com/api/user/listarUsuarios'));
     if (response.statusCode == 200) {
       return EditarUsuarios.fromJson(jsonDecode(response.body));
     } else {
@@ -41,20 +40,19 @@ class EditarUsuarios{
   }
 }
 
-class Usuarios{
+class Usuarios {
   final String id;
   final String name;
   final String email;
   final String password;
   final String rol;
 
-
-  Usuarios( this.id, {
+  Usuarios(
+    this.id, {
     required this.name,
     required this.email,
     required this.password,
     required this.rol,
-
   });
 
   factory Usuarios.fromJson(Map<String, dynamic> json) {
@@ -69,7 +67,8 @@ class Usuarios{
 
   //Realizamos el fetch a la API
   static Future<List<Usuarios>> fetchUsuarios() async {
-    final response = await http.get(Uri.parse('https://apiusuarios-copia.onrender.com/api/user/listarUsuarios'));
+    final response = await http.get(Uri.parse(
+        'https://apiusuarios-copia.onrender.com/api/user/listarUsuarios'));
     if (response.statusCode == 200) {
       List jsonResponse = jsonDecode(response.body);
       return jsonResponse.map((data) => Usuarios.fromJson(data)).toList();
@@ -79,7 +78,7 @@ class Usuarios{
   }
 
   //Realizamos el delete a la API
- static Future<void> eliminarUsuario(String id) async {
+  static Future<void> eliminarUsuario(String id) async {
     final response = await http.delete(Uri.parse(
         'https://apiusuarios-copia.onrender.com/api/user/eliminarUsuario/$id'));
     if (response.statusCode != 200) {
@@ -88,9 +87,8 @@ class Usuarios{
       //Mostramos un dialogo de cliente eeiminado con exito
     }
   }
-
-
 }
+
 class _UsuariosScreenState extends State<UsuariosScreen> {
   // Creamos los controladores para los campos de texto
   final TextEditingController _nameController = TextEditingController();
@@ -107,7 +105,6 @@ class _UsuariosScreenState extends State<UsuariosScreen> {
     editarUsuarios.fetchEditarUsuarios();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -115,72 +112,81 @@ class _UsuariosScreenState extends State<UsuariosScreen> {
       appBar: AppBar(
         title: const Text('Usuarios'),
       ),
-      body:
-      FutureBuilder<List<Usuarios>>(
+      body: FutureBuilder<List<Usuarios>>(
         future: Usuarios.fetchUsuarios(),
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            List<Usuarios> data = snapshot.data!;
             return ListView.builder(
-                itemCount: data.length,
-                itemBuilder: (BuildContext context, int index) {
-                  return ListTile(
-                    title: Text(data[index].name),
-                    subtitle: Text(data[index].email),
-                    trailing: IconButton(
-                      icon: Icon(Icons.delete),
-                      onPressed: () {
-                        Usuarios.eliminarUsuario(data[index].id);
-                      },
-                    ),
-                    onTap: () {
-                      showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          final TextEditingController _nameController = TextEditingController(text: data[index].name);
-                          final TextEditingController _emailController = TextEditingController(text: data[index].email);
-                          final TextEditingController _rolController = TextEditingController(text: data[index].rol);
-                          final id = data[index].id;
-                          return AlertDialog(
-                            title: Text('Editar Usuario'),
-                            content: Column(
-                              children: [
-                                TextField(
-                                  controller: _nameController,
-                                  decoration: InputDecoration(labelText: 'Nombre', hintText: data[index].name),
-                                ),
-                                TextField(
-                                  controller: _emailController,
-                                  decoration: InputDecoration(labelText: 'Email', hintText: data[index].email),
-                                ),
-                                TextField(
-                                  controller: _rolController,
-                                  decoration: InputDecoration(labelText: 'Rol', hintText: data[index].rol),
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(snapshot.data![index].name),
+                  subtitle: Text(snapshot.data![index].email),
+                  trailing: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      IconButton(onPressed: () {
+                        showDialog(
+                          context: context,
+                          builder: (BuildContext context) {
+                            final nameController = TextEditingController(
+                                text: snapshot.data![index].name);
+                            final emailController = TextEditingController(
+                                text: snapshot.data![index].email);
+                            final rolController = TextEditingController(
+                                text: snapshot.data![index].rol);
+
+                            return AlertDialog(
+                              title: const Text('Editar Usuario'),
+                              content: Column(
+                                children: [
+                                  TextField(
+                                    controller: nameController,
+                                    decoration: InputDecoration(
+                                    hintText: snapshot.data![index].name,
+                                    ),
+                                  ),
+                                  TextField(
+                                    controller: emailController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Email',
+                                    ),
+                                  ),
+                                  TextField(
+                                    controller: rolController,
+                                    decoration: const InputDecoration(
+                                      hintText: 'Rol',
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              actions: [
+                                TextButton(
+                                  onPressed: () {
+                                    editarUsuario(
+                                        snapshot.data![index].id, snapshot.data);
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Editar'),
                                 ),
                               ],
-                            ),
-                            actions: <Widget>[
-                              TextButton(
-                                child: Text('Guardar'),
-                                onPressed: () {
-                                  var usuarioEditado = {
-                                    'name': _nameController.text,
-                                    'email': _emailController.text,
-                                    'rol': _rolController.text,
-                                  };
-                                  editarUsuario(id, usuarioEditado);
-                                  Navigator.of(context).pop();
-                                },
-                              ),
-                            ],
-                          );
+                            );
+                          },
+                        );
+                      }, icon: Icon(Icons.edit)),
+                      IconButton(
+                        icon: const Icon(Icons.delete),
+                        onPressed: () {
+                          Usuarios.eliminarUsuario(snapshot.data![index].id);
                         },
-                      );
-                    },
-                  );
-                });
+                      ),
+                    ],
+                  ),
+                );
+              },
+            );
           } else if (snapshot.hasError) {
-            return Text('${snapshot.error}');
+            return const Text('Error');
           }
           return const CircularProgressIndicator();
         },
